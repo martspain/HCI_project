ab303009ca6b25b3ae95ce9984b4abb13f864ad7
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-05-28 18:47:22 -0600

    arreglar el push

--- src/Components/Logout/Logout.jsx
+++ src/Components/Logout/Logout.jsx
@@ -13,14 +13,10 @@ const Logout = () => {
   }
 
   return auth.currentUser && (
-<<<<<<< Updated upstream
+
     <div style = {{flexGrow:1}}>
       <button onClick={logout} style = {{width: '100%', backgroundColor:'#ffc971', height:'100%', borderWidth: '0px'}}>Cerrar sesion</button>
-=======
-    <div>
-      <button onClick={logout} style = {{flexGrow: 1}}>Cerrar sesion</button>
->>>>>>> Stashed changes
-    </div>
+</div>
   )
 }
 
